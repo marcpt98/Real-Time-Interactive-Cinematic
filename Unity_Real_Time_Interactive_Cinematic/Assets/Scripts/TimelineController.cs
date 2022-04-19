45c39a7efe511dd18c03bab123cad54cf5d2d437
marcpt98 <47535763+marcpt98@users.noreply.github.com>
2022-04-19 15:11:39 +0200

    Timeline added and working

---
+++ Unity_Real_Time_Interactive_Cinematic/Assets/Scripts/TimelineController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    
+    public void Play()
+    {
+        playableDirector.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Play();
+        }
+    }
+}
