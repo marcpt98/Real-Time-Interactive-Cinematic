f5d68ec24e95991455ab9615e006107c6c420ac0
marcpt98 <47535763+marcpt98@users.noreply.github.com>
2022-05-18 21:55:49 +0200

    Testing all animations qte in one timeline

--- Unity_Real_Time_Interactive_Cinematic/Assets/Scripts/QTE_Manager.cs
+++ Unity_Real_Time_Interactive_Cinematic/Assets/Scripts/QTE_Manager.cs
@@ -8,6 +8,7 @@ public class QTE_Manager : MonoBehaviour
     public GameObject qte_Smash;
     public GameObject qte_Simple;
     GameObject current_qte;
+    int qte_number = 0;
 
     public void CreateSmashQTE()
     {
@@ -26,13 +27,59 @@ public class QTE_Manager : MonoBehaviour
         current_qte.transform.SetParent(newCanvas.transform, false);
     }
 
+    public void GetNumberQTE(int number)
+    {
+        qte_number = number;
+        Debug.Log(number);
+    }
+
     public void Sucess()
     {
-        Debug.Log("SUCESS");
+        switch (qte_number)
+        {
+            case 1:
+                Debug.Log("1 Sucess animation playing");
+                break;
+            case 2:
+                Debug.Log("2 Sucess animation playing");
+                break;
+            case 3:
+                Debug.Log("3 Sucess animation playing");
+                break;
+            case 4:
+                Debug.Log("4 Sucess animation playing");
+                break;
+            case 5:
+                Debug.Log("5 Sucess animation playing");
+                break;
+            default:
+                Debug.LogError("WRONG NUMBER");
+                break;
+        }
     }
 
     public void Fail()
     {
-        Debug.Log("FAILED");
+        switch (qte_number)
+        {
+            case 1:
+                Debug.Log("1 Fail animation playing");
+                break;
+            case 2:
+                Debug.Log("2 Fail animation playing");
+                break;
+            case 3:
+                Debug.Log("3 Fail animation playing");
+                break;
+            case 4:
+                Debug.Log("4 Fail animation playing");
+                break;
+            case 5:
+                Debug.Log("5 Fail animation playing");
+                break;
+            default:
+                Debug.LogError("WRONG NUMBER");
+                break;
+        }
     }
 }
