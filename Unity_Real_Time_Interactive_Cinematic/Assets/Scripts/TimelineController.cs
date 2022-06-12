01518bd24b069c8dc8f41219001baa7bb0fbb90b
marcpt98 <47535763+marcpt98@users.noreply.github.com>
2022-06-12 20:35:23 +0200

    Camera prefight and Ranma fight model added. Maya scene raw done

--- Unity_Real_Time_Interactive_Cinematic/Assets/Scripts/TimelineController.cs
+++ Unity_Real_Time_Interactive_Cinematic/Assets/Scripts/TimelineController.cs
@@ -8,6 +8,7 @@ public class TimelineController : MonoBehaviour
     public PlayableDirector introTimeline;
     public PlayableDirector parkourTimeline;
     public PlayableDirector sniperwaitTimeline;
+    public PlayableDirector prefightTimeline;
     public PlayableDirector fightTimeline;
 
     // From Intro to Sniper
@@ -24,10 +25,17 @@ public class TimelineController : MonoBehaviour
         parkourTimeline.Play();
     }
 
-    // From Parkour to Fight
-    public void FightTransition()
+    // From Parkour to prefight
+    public void PrefightTransition()
     {
         parkourTimeline.Stop();
+        prefightTimeline.Play();
+    }
+
+    // From Prefight to Fight
+    public void FightTransition()
+    {
+        prefightTimeline.Stop();
         fightTimeline.Play();
     }
 }
